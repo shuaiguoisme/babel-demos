2224adb28452172f3e0af7dd8b466498f8bca68d
shuaiguoisme <shuaiguoisme@163.com>
2016-10-16 23:53:29 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "babel-polyfill";
+
+console.log(Array.from("aaa"));
+
+const sum = (a,b) => a+b;
+
+let x = sum(1,2)
+
+console.log(x)
---
+++ src/jsx.js
@@ -0,0 +1,8 @@
+import React,{Component} from "react";
+
+class DemoComponent extends Component {
+
+    render() {
+        return <div>react demo</div>
+    }
+}
